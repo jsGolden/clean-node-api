c9d48e243ef6873014087e1cf73c96af011dc035
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2023-07-17 19:22:49 -0300

    feat: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -11,14 +11,7 @@ export class DbAddAccount implements AddAccount {
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
-
-    await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
-
-    return {
-      id: '',
-      email: '',
-      name: '',
-      password: ''
-    }
+    const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    return account
   }
 }
