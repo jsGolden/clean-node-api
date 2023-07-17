7d01328c0db0f0d13e9e52a616be26b33bbfba84
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2023-07-17 18:23:04 -0300

    refactor: remove blank spaces from test suit titles

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -8,14 +8,14 @@ jest.mock('validator', () => ({
 }))
 
 describe('EmailValidator Adapter', () => {
-  test('Should return false if validator returns false ', () => {
+  test('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
 
-  test('Should return true if validator returns true ', () => {
+  test('Should return true if validator returns true', () => {
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
