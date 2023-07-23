a7f5ec647b8e35e7c82491fa9fc2672453321b1b
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2023-07-23 00:29:16 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,12 +1,16 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(global.__MONGO_URI__)
+    this.client = await MongoClient.connect(uri)
   },
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
