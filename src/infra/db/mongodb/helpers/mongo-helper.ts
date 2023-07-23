fa46c761e37ad5adda25ae142e79a55473ccd8cd
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2023-07-23 00:43:32 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient, Collection } from 'mongodb'
+import { MongoClient, Collection, InsertOneResult } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
@@ -12,5 +12,12 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map (result: InsertOneResult<Document>, data: any): any {
+    return {
+      id: result.insertedId.toString(),
+      ...data
+    }
   }
 }
