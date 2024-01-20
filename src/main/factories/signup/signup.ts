3f68820ae5daecf69d9f53de36933f04076c3d48
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2024-01-20 01:20:54 -0300

    feat: add LoginValidationFactory

---
+++ src/main/factories/signup/signup.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from '../../../presentation/controllers/signup/signup'
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { Controller } from '../../../presentation/protocols'
+import { LogControllerDecorator } from '../../decorators/log'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
+import { makeSignupValidation } from './signup-validation'
+
+export const makeSignupController = (): Controller => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const signupController = new SignUpController(dbAddAccount, makeSignupValidation())
+
+  return new LogControllerDecorator(signupController, new LogMongoRepository())
+}
