cdfee7a68c0c94139d0de0ac93673dc125176585
Wagner Rodrigues <wagner.rodrigues.dev@gmail.com>
2024-03-24 21:42:06 -0300

    refactor: change fast-glob to work with dynamic import

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,5 +4,11 @@ import fg from 'fast-glob'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
+  fg.sync('../routes/**routes.{ts,js}', {
+    cwd: __dirname,
+    ignore: [
+      '**/*.spec.*',
+      '**/*.test.*'
+    ]
+  }).map(async file => (await import(file)).default(router))
 }
