8443265e00d7311176e9626f91e0189e0a1236f8
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2024-06-10 20:53:59 -0300

    feat: add adAuthentication to SignUp factory

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -6,13 +6,18 @@ import { Controller } from '../../../presentation/protocols'
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 import { makeSignupValidation } from './signup-validation-factory'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import env from '../../config/env'
 
 export const makeSignupController = (): Controller => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signupController = new SignUpController(dbAddAccount, makeSignupValidation())
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+  const signupController = new SignUpController(dbAddAccount, makeSignupValidation(), dbAuthentication)
 
   return new LogControllerDecorator(signupController, new LogMongoRepository())
 }
