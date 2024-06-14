ac1d7065df4406f0d79bea6d8c0f31628f2a4d28
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2024-06-13 23:13:16 -0300

    chore: remove some files from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/**/protocols/*',
+    '!<rootDir>/src/main/server.ts',
+    '!<rootDir>/src/main/config/*'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
