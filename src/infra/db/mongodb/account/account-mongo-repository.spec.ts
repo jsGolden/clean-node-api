cf0e2bbc488098d1a211332ba55bb84d368e7ce4
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2024-06-16 16:22:15 -0300

    refactor: mantain loadByToken tests inside the describe flow

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -102,29 +102,29 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any_email@mail.com')
       expect(account.password).toBe('any_password')
     })
-  })
 
-  test('Should return an account on loadByToken with role', async () => {
-    const sut = makeSut()
-    await accountCollection.insertOne({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
-      accessToken: 'any_token',
-      role: 'any_role'
+    test('Should return an account on loadByToken with role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeTruthy()
+      if (!account) return
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
     })
-    const account = await sut.loadByToken('any_token', 'any_role')
-    expect(account).toBeTruthy()
-    if (!account) return
-    expect(account.id).toBeTruthy()
-    expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email@mail.com')
-    expect(account.password).toBe('any_password')
-  })
 
-  test('Should return null if loadByToken fails', async () => {
-    const sut = makeSut()
-    const account = await sut.loadByToken('any_etoken')
-    expect(account).toBeFalsy()
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_etoken')
+      expect(account).toBeFalsy()
+    })
   })
 })
