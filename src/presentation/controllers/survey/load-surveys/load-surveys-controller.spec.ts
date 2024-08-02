fb440f9a6cd6e456812e0ad3726a0d1169c73d78
Wagner Rodrigues <wagner.rodrigues@semantix.ai>
2024-08-01 21:38:32 -0300

    test: ensure LoadSurveysController returns 500 if LoadSurveysUseCase throws

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,4 +1,4 @@
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, serverError } from '../../../helpers/http/http-helper'
 import { LoadSurveysController } from './load-surveys-controller'
 import { SurveyModel, LoadSurveys } from './load-surveys-controller-protocols'
 import MockDate from 'mockdate'
@@ -68,5 +68,13 @@ describe('LoadSurveys Controller', () => {
 
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(ok(makeFakeSurveys()))
+  })
+
+  test('Should return 500 if LoadSurveys throws', async () => {
+    const { sut, loadSurveysStub } = makeSut()
+    jest.spyOn(loadSurveysStub, 'load').mockRejectedValueOnce(new Error())
+
+    const HttpResponse = await sut.handle({})
+    expect(HttpResponse).toEqual(serverError(new Error()))
   })
 })
